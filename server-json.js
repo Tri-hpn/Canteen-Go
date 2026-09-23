1054a05091d7f0948657944ae5c05f0af6d88ad5
thanh thu <tranthanhthu2607@gmail.com>
2026-09-24 01:11:32 +0700

    Add CORS for Android Capacitor

--- server-json.js
+++ server-json.js
@@ -51,6 +51,8 @@ app.use(cors({
   origin: [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "capacitor://localhost",
+    "https://localhost",
     /.vercel.app$/,
     /.onrender.com$/
   ],
